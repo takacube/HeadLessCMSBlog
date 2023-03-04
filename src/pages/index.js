65ac7587a11a2dcac5839a09432470ae3382f9be
taka <shimakuratakanao@gmail.com>
2023-03-04 23:56:37 +0900

    fix: clean up

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,59 +6,59 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const BlogIndex = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata?.title || `Title`
-  const posts = data.allMarkdownRemark.nodes
+    const siteTitle = data.site.siteMetadata?.title || `Title`
+    const posts = data.allMarkdownRemark.nodes
 
-  if (posts.length === 0) {
+    if (posts.length === 0) {
+        return (
+            <Layout location={location} title={siteTitle}>
+                <Bio />
+                <p>
+                No blog posts found. Add markdown posts to "content/blog" (or the
+                directory you specified for the "gatsby-source-filesystem" plugin in
+                gatsby-config.js).
+                </p>
+            </Layout>
+        )
+    }
+
     return (
-      <Layout location={location} title={siteTitle}>
+        <Layout location={location} title={siteTitle}>
         <Bio />
-        <p>
-          No blog posts found. Add markdown posts to "content/blog" (or the
-          directory you specified for the "gatsby-source-filesystem" plugin in
-          gatsby-config.js).
-        </p>
-      </Layout>
+        <ol style={{ listStyle: `none` }}>
+            {posts.map(post => {
+            const title = post.frontmatter.title || post.fields.slug
+
+            return (
+                <li key={post.fields.slug}>
+                <article
+                    className="post-list-item"
+                    itemScope
+                    itemType="http://schema.org/Article"
+                >
+                    <header>
+                    <h2>
+                        <Link to={post.fields.slug} itemProp="url">
+                        <span itemProp="headline">{title}</span>
+                        </Link>
+                    </h2>
+                    <small>{post.frontmatter.date}</small>
+                    </header>
+                    <section>
+                    <p
+                        dangerouslySetInnerHTML={{
+                        __html: post.frontmatter.description || post.excerpt,
+                        }}
+                        itemProp="description"
+                    />
+                    </section>
+                </article>
+                </li>
+            )
+            })}
+        </ol>
+        </Layout>
     )
-  }
-
-  return (
-    <Layout location={location} title={siteTitle}>
-      <Bio />
-      <ol style={{ listStyle: `none` }}>
-        {posts.map(post => {
-          const title = post.frontmatter.title || post.fields.slug
-
-          return (
-            <li key={post.fields.slug}>
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-              >
-                <header>
-                  <h2>
-                    <Link to={post.fields.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
-                    </Link>
-                  </h2>
-                  <small>{post.frontmatter.date}</small>
-                </header>
-                <section>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html: post.frontmatter.description || post.excerpt,
-                    }}
-                    itemProp="description"
-                  />
-                </section>
-              </article>
-            </li>
-          )
-        })}
-      </ol>
-    </Layout>
-  )
 }
 
 export default BlogIndex
@@ -71,24 +71,24 @@ export default BlogIndex
 export const Head = () => <Seo title="All posts" />
 
 export const pageQuery = graphql`
-  {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMarkdownRemark(sort: { frontmatter: { date: DESC } }) {
-      nodes {
-        excerpt
-        fields {
-          slug
+    {
+        site {
+        siteMetadata {
+            title
         }
-        frontmatter {
-          date(formatString: "MMMM DD, YYYY")
-          title
-          description
         }
-      }
+        allMarkdownRemark(sort: { frontmatter: { date: DESC } }) {
+        nodes {
+            excerpt
+            fields {
+            slug
+            }
+            frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+            }
+        }
+        }
     }
-  }
 `
