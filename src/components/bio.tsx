9e6b26930c474ded0d9a776c1047f0fa3678ea7f
taka <shimakuratakanao@gmail.com>
2023-03-05 11:22:01 +0900

    fix: nit fix

--- src/components/bio.tsx
+++ src/components/bio.tsx
@@ -36,7 +36,7 @@ const Bio = () => {
             className="bio-avatar"
             layout="fixed"
             formats={["auto", "webp", "avif"]}
-            src="../images/profile-pic.png"
+            src="../images/gatsby-icon.png"
             width={50}
             height={50}
             quality={95}
@@ -46,8 +46,8 @@ const Bio = () => {
             <p>
             Written by <strong>{author.name}</strong> {author?.summary || null}
             {` `}
-            <a href={`https://twitter.com/${social?.twitter || ``}`}>
-                You should follow them on Twitter
+            <a href={`https://github.com/takacube`}>
+                You should follow them on Github
             </a>
             </p>
         )}
